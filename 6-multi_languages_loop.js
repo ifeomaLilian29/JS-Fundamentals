bad249d29888ccfc7bf03541ab1506bc7d1942d5
Ifeoma <mizfee09@gmail.com>
2025-05-25 09:02:10 +0100

    Fixed errors in 6-multi_languages_loop.js

--- 6-multi_languages_loop.js
+++ 6-multi_languages_loop.js
@@ -1,6 +1,9 @@
 const messages = ["C is fun", "Python is cool", "JavaScript is amazing"];
+let result = "";
 for(let i=0; i < messages.length; i++) {
-    console.log(messages[i]);
-
+    result += messages[i];
+    if (i < messages.length -1) {
+        result += "\n";
+    }
 }
-    
+  console.log(result);
